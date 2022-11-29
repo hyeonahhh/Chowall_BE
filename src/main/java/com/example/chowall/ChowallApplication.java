04f716ffa664555cdbd98c6507bbbe947abd47f9
hyeonah <hyeona0000@naver.com>
2022-11-29 11:34:17 +0900

    221129 초 setting

---
+++ src/main/java/com/example/chowall/ChowallApplication.java
@@ -0,0 +1,13 @@
+package com.example.chowall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChowallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChowallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chowall/ChowallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chowall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChowallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
